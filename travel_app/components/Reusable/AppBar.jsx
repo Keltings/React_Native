3a89f94e4c846b82f6164e65a71cbec1c929ec9e
Keltings <lkelida@gmail.com>
2024-01-24 22:41:54 +0300

    search and arrow bars

--- travel_app/components/Reusable/AppBar.jsx
+++ travel_app/components/Reusable/AppBar.jsx
@@ -1,14 +1,63 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import reusable from '../../components/Reusable/reusable.style'
+import { AntDesign } from '@expo/vector-icons';
+import ReusableText from './ReusableText';
+import { COLORS, SIZES, TEXT } from '../../constants/theme';
 
-const AppBar = () => {
+const AppBar = ({color, title, color1}) => {
   return (
-    <View>
-      <Text>AppBar</Text>
+    <View style={styles.overlay}>
+      <View style={reusable.rowWithSpace('space-between')}>
+        <TouchableOpacity style={styles.box(color)}>
+          <AntDesign 
+          name='left'
+          size={26}   
+          />
+        </TouchableOpacity>
+
+        <ReusableText
+                  text={title}
+                  family={"medium"}
+                  size={TEXT.large}
+                  color={COLORS.black}
+            />
+        <TouchableOpacity style={styles.box1(color1)}>
+          <AntDesign 
+          name='search1'
+          size={26}   
+          />
+        </TouchableOpacity>
+
+      </View>
+
     </View>
   )
 }
 
 export default AppBar
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  overlay: {
+    position: "absolute",
+    top: 10,
+    left: 0,
+    right:0,
+    justifyContent: 'center',
+  },
+  box: (color) => ({ 
+    backgroundColor:color,
+    width: 30,
+    height: 30, 
+    borderRadius:9,
+    alignItems: 'center'
+  }),
+
+  box1: (color) => ({ 
+    backgroundColor:color1,
+    width: 30,
+    height: 30, 
+    borderRadius:9,
+    alignItems: 'center'
+  }),
+});
--- travel_app/screens/details/Recommended.jsx
+++ travel_app/screens/details/Recommended.jsx
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import AppBar from '../../components/Reusable/AppBar'
 
 const Recommended = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{marginHorizontal:20}}>
       <View style={{height: 80}}>
+        <AppBar  title={'Recommendation'} color={COLORS.white}/>
 
       </View>
     </SafeAreaView>
