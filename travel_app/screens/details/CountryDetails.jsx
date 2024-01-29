2d120744b71bbca0948828212e0ecff11eedb35b
Keltings <lkelida@gmail.com>
2024-01-29 23:33:01 +0300

    Country details

--- travel_app/screens/details/CountryDetails.jsx
+++ travel_app/screens/details/CountryDetails.jsx
@@ -1,8 +1,11 @@
-import { StyleSheet, View, Text, ScrollView } from 'react-native'
+import { StyleSheet, View, Text, ScrollView, Touchable, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { useRoute } from '@react-navigation/native'
 import { SafeAreaView } from 'react-native-safe-area-context';
-import {NetworkImage, AppBar} from '../../components/index'
+import {NetworkImage, AppBar, ReusableText, DescriptionText, ReusableBtn, PopularList, HeightSpacer} from '../../components/index'
+import { COLORS, TEXT, SIZES } from '../../constants/theme';
+import reusable from '../../components/Reusable/reusable.style';
+import {Feather} from '@expo/vector-icons'
 
 const CountryDetails = ({navigation}) => {
   const route = useRoute();
@@ -34,27 +37,75 @@ const CountryDetails = ({navigation}) => {
 }
   console.log(item);
   return (
-    <SafeAreaView style={styles.container}>
       <ScrollView>
         <View>
           <NetworkImage 
           source={country.imageUrl}
-          width={'100'}
+          width={'100%'}
           height={350}
           radius={30}
           />
 
         <AppBar  
+          top={40}
+          left={20}
+          right={20}
           title={country.country} 
           color={COLORS.white} 
           icon={'search1'} 
           color1={COLORS.white}
           onPress={()=> navigation.goBack()}
-          onPress1={() => {}}
+          onPress1={() => {
+            
+          }}
         />
         </View>
+
+        <View style={styles.description}>
+        <ReusableText
+                  text={country.region}
+                  family={"medium"}
+                  size={TEXT.xLarge}
+                  color={COLORS.black}
+        />
+        <DescriptionText text={country.description} />
+
+
+        <View style={{alignContent: 'center'}}>
+
+          <View style={reusable.rowWithSpace("space-between")}>
+          <ReusableText
+                  text={"Popular Destinations"}
+                  family={"medium"}
+                  size={TEXT.large}
+                  color={COLORS.black}
+          />
+
+          <TouchableOpacity onPress={()=> {}}>
+            <Feather 
+            name='list'
+            size={20}
+            />
+          </TouchableOpacity>
+          </View>
+          <HeightSpacer height={20}/>
+          <PopularList data={country.popular}/>
+
+          <ReusableBtn 
+                  onPress={()=> navigation.navigate('HotelSearch', id)}
+                  btnText={"Find Best Hotels"}
+                  width={SIZES.width - 40}
+                  backgroundColor={COLORS.green}
+                  borderColor={COLORS.green}
+                  borderWidth={0}
+                  textColor={COLORS.white}
+                />
+
+          <HeightSpacer height={50}/>
+
+        </View>
+        </View>
       </ScrollView>
-    </SafeAreaView>
   )
 }
 
@@ -62,6 +113,9 @@ export default CountryDetails
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#F3F4F8',
-    marginHorizontal: 20
+  },
+  description: {
+    marginHorizontal:20,
+    paddingTop: 20
   }
 })
